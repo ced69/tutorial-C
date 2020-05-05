7e2a33617161206e2f4c79bd5aa9a50f8ab203dc
Ced <ced_69@hotmail.fr>
2020-05-05 17:09:29 +0200

    exo08

---
+++ data/08/main.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char *argv[])
+{
+int majeur = 1;
+
+if (majeur)
+{
+	printf("Tu es majeur !\n");
+}
+else
+{
+	printf("Tu es mineur !");
+}
+
+	return 0;
+}
+
---
+++ data/09/main.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main (int argc, char *argv[])
+{
+	switch (age)
+{
+case 2:
+	printf("Salut bebe !\n");
+	break;
+case 6:
+	printf("Salut gamin !\n");
+	break;
+case 12:
+	printf("Salut l'ado !\n");
+	break;i
+case 16:
+	printf("Toujous puceau ?\n");
+	break;
+case 18:
+	printf("C'est bon tu es majeur !!\n");
+case 20:
+	printf("Alors t'as le permis ?\n");
+case 70:
+	printf("Salut papy, ca va ?\n");
+	break;
+default:
+	printf("Je n'ai aucune réponse pour ton age\n");
+	break;
+}
+	return 0;
+}
+
+
+
