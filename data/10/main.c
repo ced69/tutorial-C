097c277c24807d6da2d0bca5b508b4de407a3b34
Ced <ced_69@hotmail.fr>
2020-05-06 14:50:19 +0200

    exo10 commande Mc Do

--- data/10/main.c
+++ data/10/main.c
@@ -3,42 +3,45 @@
 
 int main (int argc, char *argv[])
 {
-  int choixMenu;
+	int choixMenu;
+	
+	printf("===Menu===\n\n");
+	printf("1. Royal Cheese\n");
+	printf("2. Mc Deluxe\n");
+	printf("3. Mc Bacon\n");
+	printf("4. Big Mac\n");
+	printf("5. Nuggets x 12\n");
+	printf("Votre Choix ?\n");
+	scanf("%d", &choixMenu);
+	
+	printf("\n");
 
-  printf("===Menu===\n\n");
-  printf("1. Royal Cheese\n");
-  printf("2. Mc Deluxe\n");
-  printf("3. Mc Bacon\n");
-  printf("4. Big Mac\n");
-  printf("5. Nuggets x 12\n");
-  scanf("%d", &choixMenu);
+	
+	
+	switch (choixMenu)
+	{
+		case 1:
+			printf("Vous avez choisi le Royal Cheese. Bon choix !`\n");
+			break;
+		case 2:
+			printf("Vous avez choisi le Mc Deluxe, pas le top !!\n");
+			break; 
+		case 3:
+			printf("Vous avez choisi le Mc Bacon, vraiment bon !\n");
+			break;
+		case 4:
+			printf("Vous avez choisi le Big Mac, faut essayer avec du bacon !!\n");
+			break;
+		case 5:
+			printf("Des nuggets, je vous conseille la sauce BBQ\n");
+			break;
+		default:
+			printf("Vous n'avez pas rentre un nom correct, vous mangerez vos doigts\n");	    
+	}
 
-  printf("\n");
+	printf("\n \n");
 
-  switch (choixMenu)
-  {
-    case 1:
-	    printf("Vous avez choisi le Royal Cheese. Bon choix !`\n");
-	    break;
-    case 2:
-	    printf("Vous avez choisi le Mc Deluxe, pas le top !!\n");
-	    break; 
-    case 3:
-	    printf("Vous avez choisi le Mc Bacon, vraiment bon !\n");
-	    break;
-    case 4:
-	    printf("Vous avez choisi le Big Mac, faut essayer avec du bacon !!\n");
-	    break;
-    case 5:
-	    printf("Des nuggets, je vous conseille la sauce BBQ\n");
-	    break;
-    default:
-	   printf("Vous n'avez pas rentre un nom correct, vous mangerez vos doigts\n");	    
-  }
-
-  printf("\n \n");
-
-  return 0;
+	return 0;
 }
 
 
