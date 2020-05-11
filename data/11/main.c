bfd50ab94fc461f66063d8fd47164ff8bc1c3d61
Ced <ced_69@hotmail.fr>
2020-05-11 18:59:32 +0200

    if..else,fonctionne pas

---
+++ data/11/main.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main (int argc, char *argv[])
+{
+	bool majeur = false;
+
+	if (age >=18) 
+		{
+		majeur = true;
+		}
+		else
+		{
+		majeur = false;
+		}
+
+		return 0;
+
+}
+
