1b09f36e84e325c86ab5160fe8e43809853bd875
Ced <ced_69@hotmail.fr>
2020-05-11 15:10:14 +0200

    exo12 les boucles

---
+++ data/12/main.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main( int argc, char *argv[])
+{
+	int nombreEntre = 0;
+
+	printf("ca marche pas !!");
+
+	while (nombreEntre != 47);
+	{
+		printf("Tapez le nombre 47 !");
+		scanf("%d", &nombreEntre);
+	}
+	return 0;
+}
+
