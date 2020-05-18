f988ffe51afb1308c8842069a41c252463131977
Ced <ced_69@hotmail.fr>
2020-05-18 13:37:47 +0200

    exo15

---
+++ data/15/main.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main (int argc, char *argv[])
+
+{
+	int compteur = 0;
+
+	do
+	{
+		printf("Salut les Zeros !\n");
+		compteur++;
+	} while (compteur < 10);
+
+	return 0;
+}
+
