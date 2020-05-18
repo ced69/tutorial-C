9b5a5535930398c62fedc4e3e837c9310a2c6696
Ced <ced_69@hotmail.fr>
2020-05-18 13:38:02 +0200

    exo16

---
+++ data/16/main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main (int arg, char *argv[])
+
+{
+	int compteur;
+
+	for (compteur = 0 ; compteur < 10 ; compteur++)
+	{
+		printf("Salut les Zeroz !\n");
+	}
+
+	return 0;
+}
+
