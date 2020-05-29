781c079257d83af92654f38d2e702c2080c143bf
Ced <ced_69@hotmail.fr>
2020-05-29 19:23:34 +0200

    le menu avec une boucle

--- data/22/main.c
+++ data/22/main.c
@@ -4,9 +4,10 @@
 int menu()
 {
 	int choix = 0;
-
+		
 	while (choix < 1 || choix > 4)
 	{
+
 		printf("Menu :\n");
 		printf("1 : Poulet de dinde aux escargots rotis a la sauce bearnaise\n");
 		printf("2 : Concombres sucres a la sauce de myrtlles enrobee de chocolat\n");
@@ -14,16 +15,24 @@ int menu()
 		printf("4 : La suprise du Chef\n");
 		printf("Votre choix ? ");
 		scanf("%d", &choix);
+		if (choix < 1 || choix > 4)
+		{
+			printf("Vous avez tape un numero qui n'est pas dans le menu !!\n");
+		}
 
 	}
+
 
 	return choix;
 }
 
 int main(int argc, char *argv[])
-
 {
-	switch(menu())
+	int choix = 0;
+	choix = menu();
+	
+
+	switch(choix)
 	{
 		case 1 :
 			printf("Vous avez pris le poulet\n");
@@ -37,13 +46,10 @@ int main(int argc, char *argv[])
 		case 4 :
 			printf("Vous avez choisi la surprise du Chef\n");
 			break;
+		default :
+			printf("Vous avez fait un mauvais choix\n");	
+			break;	
 	}
 	return 0;
 }
-
-
-
-
-
-
 
